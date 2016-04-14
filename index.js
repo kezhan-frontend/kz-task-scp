a3f113b25126c178ec35346c1f4f8cbd3c55620c
wyicwx <wyicwx@gmail.com>
2016-04-14 11:17:33 +0800

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+var client = require('scp2');
+
+module.exports = function(options) {
+    options || (options = {});
+
+    if(!options.server || !options.path) {
+        throw new Error('options.server && options.path are required!');
+    }
+
+    return function() {
+        var staticPath = this.fs.pathResolve(options.path);
+
+        bone.log('scp', 'start sync to server: '+options.server.host+':'+options.server.path);
+
+        client.scp(static, options.server, function(err) {
+            if(err) {
+                throw err;
+            } else {
+                bone.log.info('scp','upload '+options.path+' success!');
+            }
+        });
+    };
+};
